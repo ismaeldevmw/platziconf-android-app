185e17fb88ee29bbc2c6ab9e78258f9cbc431c23
Ismael López <lc.ismael@hotmail.com>
2020-04-20 17:09:01 -0500

    :sparkles: Implemented Google Maps
    Created  ma view  and added a marker into map

--- app/src/main/java/com/platzi/conf/model/Ubication.kt
+++ app/src/main/java/com/platzi/conf/model/Ubication.kt
@@ -1,11 +1,11 @@
 package com.platzi.conf.model
 
 class Ubication {
-    val name = ""
-    val address = ""
-    val latitude = ""
-    val longitude = ""
-    val phone = ""
-    val website = ""
-    val photo = ""
+    val name = "Centro Cultural Roberto Cantoral"
+    val address = "Puente Xoco s/n, Xoco, 03330 Ciudad de México, CDMX"
+    val latitude = 19.362028
+    val longitude = -99.166414
+    val phone = "+52 55 1519 4670"
+    val website = "http://elcantoral.com"
+    val photo = "http://lh5.googleusercontent.com/p/AF1QipPj-rZ_uZmMzvQT3iVlA832hei26T_qjFbjYiW6=w408-h270-k-no"
 }
